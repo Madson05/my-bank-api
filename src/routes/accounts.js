3f1ecd3ae59875c916e16e485d73a25b54bfa6f7
madson <madsonsilva2005@gmail.com>
2023-01-24 16:08:49 -0300

    implementado metodos put e patch para alteração dos dados e refatorado o tratamento de erros

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-import { promises as fs } from "fs";
+import { promises as fs, write } from "fs";
 
 const { readFile, writeFile } = fs;
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
   try {
     let account = req.body;
 
@@ -19,31 +19,83 @@ router.post("/", async (req, res) => {
     await writeFile(global.fileName, JSON.stringify(data, null, 2));
     res.send(account);
   } catch (error) {
-    res.status(400).send({ error: error.message });
+    next(error)
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.fileName));
     delete data.nextId;
     res.send(data);
   } catch (error) {
-    res.status(400).send({ error: error.message });
+    next(error)
   }
 });
 
-router.get("/:id", async (req, res) => {
-  try{
-    const data = JSON.parse(await readFile(global.fileName))
+router.get("/:id", async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
     const account = data.accounts.find((account) => {
-      return account.id === parseInt(req.params.id)
-    })
+      return account.id === parseInt(req.params.id);
+    });
 
-    res.send(account)
-  }catch(e){
-
+    res.send(account);
+  } catch (error) {
+    next(error)
   }
 });
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    data.accounts = data.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id)
+    );
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    res.end();
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.put("/", async (req, res, next) => {
+  try {
+    const account = req.body;
+
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+
+    data.accounts[index] = account;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+    res.end();
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.patch("/updateBalance", async (req, res) => {
+  try {
+    const account = req.body;
+
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex((a) => a.id === account.id);
+
+    data.accounts[index].balance = account.balance;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+    res.end();
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.use((error, req, res, next) => {
+  console.log(error)
+  res.status(400).send({ error: error.message });
+})
 
 export default router;
